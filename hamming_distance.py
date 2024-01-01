8d48a4549fac441a9c8a9ba12f96e3ee5efdd438
Nick-Kevin <nicblou23@gmail.com>
2024-01-01 03:34:46 +0300

    update hamming_distance

---
+++ hamming_distance.py
@@ -0,0 +1,6 @@
+print("\n This script calculates hamming distance between two ADN sequences. \n")
+
+FirstSequence = input("Enter the first sequence: ")
+print("The first sequence you enter is:", FirstSequence, "\n")
+SecondSequence = input("Enter the second sequence: ")
+print("The second sequence you enter is:", SecondSequence)
