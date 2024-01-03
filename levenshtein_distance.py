ae5713c3637ee1e7602b7aca834c5f62907ad792
Nick-Kevin <nicblou23@gmail.com>
2024-01-03 12:14:47 +0300

    update levenshtein_distance

--- levenshtein_distance.py
+++ levenshtein_distance.py
@@ -14,7 +14,12 @@ def minimum(arg1, arg2, arg3):
 
 def levenshtein_distance(firstSequence, secondSequence):
     Rows, Cols = (len(firstSequence), len(secondSequence))
-    Distance = [[0] * Cols] * Rows
+    Distance = [[] for _ in range(len(secondSequence))]
+    Distance[0].append(0)
+    for index in range(1, len(secondSequence), 1):
+        Distance[index].append(Distance[index-1][0] + 1)
+    for index in range(1, len(firstSequence), 1):
+        Distance[0].append(Distance[0][index-1]+1)
     print(Distance)
 
 levenshtein_distance(FirstSequence, SecondSequence)
