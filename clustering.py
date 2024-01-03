4ca72fd58719437154dde133cdded0f6641ff33b
Nick-Kevin <nicblou23@gmail.com>
2024-01-03 16:07:06 +0300

    update clustering

--- clustering.py
+++ clustering.py
@@ -1,6 +1,23 @@
 from levenshtein_distance import levenshtein_distance
 
-FirstSequence = input("enter the first sequence")
-SecondSequence = input("enter the second sequence")
+NumberOfSequences = int(input("Enter the number of ADN sequences: "))
 
-levenshtein_distance(FirstSequence, SecondSequence)
+Sequences = []
+for _ in range(NumberOfSequences):
+    Sequence = input("Enter the ADN sequence " + str(_+1) + ": ")
+    Sequences.append(Sequence)
+
+print("All sequences you'd enter:", Sequences)
+
+DistancesBetweenSequences = []
+for rowSequence in Sequences:
+    CurrentRow = []
+    for colSequence in Sequences:
+        distance = levenshtein_distance(rowSequence, colSequence)
+        CurrentRow.append(distance)
+    DistancesBetweenSequences.append(CurrentRow)
+
+for Row in DistancesBetweenSequences:
+    for Column in Row:
+        print(Column, end=" ")
+    print()
