e12dd93ea8847a291d8b62085d03a7883f2c13c2
Nick-Kevin <nicblou23@gmail.com>
2024-01-03 15:09:22 +0300

    create clustering.py

---
+++ clustering.py
@@ -0,0 +1,6 @@
+from levenshtein_distance import levenshtein_distance
+
+FirstSequence = input("enter the first sequence")
+SecondSequence = input("enter the second sequence")
+
+levenshtein_distance(FirstSequence, SecondSequence)
