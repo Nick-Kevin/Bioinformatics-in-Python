2ce5b7a408c0a2f99c85f9f919a37a92bd6931fc
Nick-Kevin <nicblou23@gmail.com>
2024-01-03 14:26:36 +0300

    update levenshtein_distance

--- levenshtein_distance.py
+++ levenshtein_distance.py
@@ -13,24 +13,15 @@ def minimum(arg1, arg2, arg3):
     else: return arg3
 
 def levenshtein_distance(firstSequence, secondSequence):
-    Distance = [[] for _ in range(len(secondSequence)+1)]
+    Distance = [[] for _ in range(len(secondSequence)+1)]    
     Distance[0].append(0)
-    for RowIndex in range(1, len(secondSequence), 1):
+    for RowIndex in range(1, len(secondSequence)+1, 1):
         Distance[RowIndex].append(Distance[RowIndex-1][0] + 1)
-    for ColumnIndex in range(1, len(firstSequence), 1):
-        Distance[0].append(Distance[0][ColumnIndex-1]+1)    
-    """for row in Distance:
-        for column in row:
-            if firstSequence[column] != secondSequence[Distance.index(row)]:
-                dist1 = Distance[Distance.index(row)-1][column-1] + 1
-            else:
-                dist1 = Distance[Distance.index(row)-1][column-1]
-            dist2 = Distance[Distance.index(row)-1][column] + 1
-            dist3 = Distance[Distance.index(row)][column-1] + 1
-            Distance[Distance.index(row)].append(minimum(dist1, dist2, dist3))"""
-    for RowIndex in range(1, len(Distance)-1):
-        for ColumnIndex in range(1, len(firstSequence)):
-            if firstSequence[ColumnIndex] != secondSequence[RowIndex]:
+    for ColumnIndex in range(1, len(firstSequence)+1, 1):
+        Distance[0].append(Distance[0][ColumnIndex-1]+1)
+    for RowIndex in range(1, len(Distance)):
+        for ColumnIndex in range(1, len(firstSequence)+1):
+            if firstSequence[ColumnIndex-1] != secondSequence[RowIndex-1]:
                 dist1 = Distance[RowIndex-1][ColumnIndex-1] + 1
             else:
                 dist1 = Distance[RowIndex-1][ColumnIndex-1]
