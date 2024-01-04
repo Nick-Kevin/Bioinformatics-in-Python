dd871fa87b6dc6dc368fdfa2b9972b1589b0e93a
Nick-Kevin <nicblou23@gmail.com>
2024-01-04 15:11:36 +0300

    update clustering.py: create distance_between_classes, is_class, distance_between_class_sequence functions

--- clustering.py
+++ clustering.py
@@ -7,8 +7,6 @@ def find(table, value):
                 return { 'row': table.index(Row), 'col': Row.index(value)}
     return { 'row': -1, 'col': -1 }
 
-NumberOfSequences = int(input("Enter the number of sequences: "))
-
 def get_sequences_by_distance(table, distance_value):
     Position = find(table, distance_value)
     ColumnOfTheDistance = Position['col']
@@ -28,36 +26,71 @@ def is_finished(table, sequences_table):
         return False
     return True
 
+def is_class(value):
+    if isinstance(value, list) == False:
+        return False
+    return True
 
-Sequences = []
-for i in range(NumberOfSequences):
-    sequence = input("Entrer the ADN sequence " + str(i+1) + ": ")
-    Sequences.append(sequence)
+def distance_between_class_and_sequence(classTable, sequence):
+    Distance = []
+    for item in classTable:
+        dist = levenshtein_distance(item, sequence)
+        Distance.append(dist)
+    minimum = min(Distance)
+    return minimum
 
-print("All sequences you entered: ", Sequences)
+def distance_between_classes(firstClassTable, secondClassTable):
+    Distance = []
+    for sequence in secondClassTable:
+        dist = distance_between_class_and_sequence(firstClassTable, sequence)
+        Distance.append(dist)
+    minimum = min(Distance)
+    return minimum
 
-DistanceTable = []
-Classes = []
-FirstRow = []
-FirstRow.append("")
-for sequence in Sequences:
-    FirstRow.append(sequence)
-print(FirstRow  )
-DistanceTable.append(FirstRow)
+def merge_classes(firstClassTable, secondClassTable):
+    for sequence in secondClassTable:
+        firstClassTable.append(sequence)
+    return firstClassTable
 
-for sequence in Sequences:
-    NewRow = []
-    NewRow.append(sequence)
-    for other_sequence in Sequences:
-        Distance = levenshtein_distance(sequence, other_sequence)
-        NewRow.append(Distance)
-    DistanceTable.append(NewRow)
+def main():
+    NumberOfSequences = int(input("Enter the number of sequences: "))
+    Sequences = []
+    for i in range(NumberOfSequences):
+        sequence = input("Entrer the ADN sequence " + str(i+1) + ": ")
+        Sequences.append(sequence)
 
-for Row in DistanceTable:
-    print(Row)
+    print("All sequences you entered: ", Sequences)
 
-Value = int(input("Enter the distance between the two sequence: "))
+    DistanceTable = []
+    Classes = []
+    FirstRow = []
+    FirstRow.append("")
+    for sequence in Sequences:
+        FirstRow.append(sequence)
+    print(FirstRow  )
+    DistanceTable.append(FirstRow)
 
-SequencesOfTheValue = get_sequences_by_distance(DistanceTable, Value)
-print('First sequence: ', SequencesOfTheValue['first_sequence'])
-print('Second sequence: ', SequencesOfTheValue['second_sequence'])
+    for sequence in Sequences:
+        NewRow = []
+        NewRow.append(sequence)
+        for other_sequence in Sequences:
+            Distance = levenshtein_distance(sequence, other_sequence)
+            NewRow.append(Distance)
+        DistanceTable.append(NewRow)
+
+    for Row in DistanceTable:
+        print(Row)
+
+    Value = int(input("Enter the distance between the two sequence: "))
+
+    SequencesOfTheValue = get_sequences_by_distance(DistanceTable, Value)
+    print('First sequence: ', SequencesOfTheValue['first_sequence'])
+    print('Second sequence: ', SequencesOfTheValue['second_sequence'])
+
+#main()
+tab = ["aa","bb", "cc"]
+checkTab = is_class(['a'])
+if checkTab:
+    print("it's an array")
+else:
+    print("it's not array")
