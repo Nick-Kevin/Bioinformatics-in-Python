8db917414e8a68c1f6caef719e13bb80228eac82
Nick-Kevin <nicblou23@gmail.com>
2024-01-05 17:50:18 +0300

    update most nearby neighbour

--- most_nearby_neighbour.py
+++ most_nearby_neighbour.py
@@ -1,15 +1,17 @@
 from levenshtein_distance import levenshtein_distance
+from color import color
 
-NumberOfSequences = int(input("Enter the number of ADN sequences: "))
+NombreDeSequences = int(input("Entrez le nombre de séquences d'ADN à classer: "))
 
 Sequences = []
-for _ in range(NumberOfSequences):
-    Sequence = input("Enter the ADN sequence " + str(_+1) + ": ")
+for _ in range(NombreDeSequences):
+    Sequence = input("Entrez la séquence d'ADN numéro " + str(_+1) + ": ")
     Sequences.append(Sequence)
 
-print("All sequences you entered:", Sequences)
+print("\nLes séquences que vous avez entré:" + str(Sequences) + "\n")
 
-DoorStep = int(input("Enter the doorstep: "))
+# implémentation de l'algorithme
+Seuil = int(input("Entrer le seuil t: "))
 Classes = []
 Classe1 = []
 Classe1.append(Sequences[0])
@@ -17,27 +19,30 @@ Classes.append(Classe1)
 j = 1
 m = 1
 while j < len(Sequences):
-    DansUneClasse = False   
+    Distances = []
     for classe in Classes:
-        Distancese = []
+        DistancesEntreLesSequences = []     
         for sequence in classe:
             distance = levenshtein_distance(sequence, Sequences[j])
-            Distancese.append(distance)
-        minD = Distancese[0]
-        for dist in Distancese:
-            if dist < minD:
-                minD = dist
-        if minD <= DoorStep:
-            classe.append(Sequences[j])
-            DansUneClasse = True
-            break
-    if DansUneClasse == False:
-        Classes.append([]) # new class for Sequences[j]
-        Classes[m].append(Sequences[j]) # insert the sequence in the new class
+            DistancesEntreLesSequences.append(distance)
+        DistanceEntreLaClasseEtLaSequence = min(DistancesEntreLesSequences)
+        Distances.append(DistanceEntreLaClasseEtLaSequence)
+    DistanceMinimale = min(Distances)
+    PositionDeLaDistanceMinimal = Distances.index(DistanceMinimale)
+    if DistanceMinimale <= Seuil:
+        ClassDeLaSequence = Classes[PositionDeLaDistanceMinimal]
+        ClassDeLaSequence.append(Sequences[j])
+    else:
+        Classes.append([]) # nouvelle classe pour Sequences[j]
+        Classes[m].append(Sequences[j]) # insertion de la sequence dans la nouvelle classe
+        m += 1
     j += 1
 
+#Affichage
+print()
 for classe in Classes:
-    GroupeClass = "Classe " + str(Classes.index(classe)+1) + ":"
+    GroupeClass = color.UNDERLINE + "Classe " + str(Classes.index(classe)+1) + ":" + color.END
     for sequence in classe:
         GroupeClass += " " + sequence + ", "
     print(GroupeClass)
+print()
