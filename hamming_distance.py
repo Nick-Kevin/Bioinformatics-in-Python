624bb23e377d0d9f353bc2a507d566e675936cab
Nick-Kevin <nicblou23@gmail.com>
2024-01-05 16:54:24 +0300

    updte hamming

--- hamming_distance.py
+++ hamming_distance.py
@@ -1,19 +1,18 @@
-print("\nThis module calculates hamming distance between two ADN sequences.")
+print("\nCe module calcul la distance de Hamming entre deux séquence d'ADN entrer par l'utilisatuer.")
 
 while True:
-    print("\nNB: The first and second sequences mmust have the same length.")
-    FirstSequence = input("\tEnter the first sequence: ")
-    SecondSequence = input("\tEnter the second sequence: ")
-    if len(FirstSequence) == len(SecondSequence):
+    print("\nNB: Le nombre de nucléotide des deux séquences doivent être égale.")
+    PremiereSequence = input("\tEntrez la première séquence: ")
+    DeuxièmeSequence = input("\tEntrez la deuxième séquence: ")
+    if len(PremiereSequence) == len(DeuxièmeSequence):
         break
 
-print("The first sequence is:", FirstSequence,", and the second one is:", SecondSequence)
-
 Distance = 0
-for nucleotide in range(len(FirstSequence)):
-    if FirstSequence[nucleotide] != SecondSequence[nucleotide]:
+for nucleotide in range(len(PremiereSequence)):
+    if PremiereSequence[nucleotide] != DeuxièmeSequence[nucleotide]:
         Distance += 1
 
-print("\nThe hamming distance between '" + FirstSequence +"' and '" + SecondSequence + "' is:")
-print("d(" + FirstSequence + "," + SecondSequence + ") =", Distance)
+print("\nThe hamming distance between '" + PremiereSequence +"' and '" + DeuxièmeSequence + "' is:")
+print("Dh(" + PremiereSequence + "," + DeuxièmeSequence + ") =", Distance)
 print("")
+
